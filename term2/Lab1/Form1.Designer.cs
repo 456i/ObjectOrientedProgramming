a797f93db966ccc58d13ad34456e64002c262b1d
456i <timofeymarushko@gmail.com>
2026-02-16 13:16:21 +0300

    completed lab1

---
+++ term2/Lab1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.calculateBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Длина";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(48, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(94, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Ширина";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(48, 117);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(94, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Высота";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // calculateBTN
+            // 
+            this.calculateBTN.Location = new System.Drawing.Point(48, 161);
+            this.calculateBTN.Name = "calculateBTN";
+            this.calculateBTN.Size = new System.Drawing.Size(94, 20);
+            this.calculateBTN.TabIndex = 3;
+            this.calculateBTN.Text = "Рассчитать";
+            this.calculateBTN.UseVisualStyleBackColor = true;
+            this.calculateBTN.Click += new System.EventHandler(this.calculateBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 399);
+            this.Controls.Add(this.calculateBTN);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button calculateBTN;
+    }
+}
+
---
+++ term2/Lab1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateBTN_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text, out double length) ||
+                !double.TryParse(textBox2.Text, out double width) ||
+                !double.TryParse(textBox3.Text, out double height))
+            {
+                MessageBox.Show("Введите корректные числовые значения.");
+                return;
+            }
+
+            if (length <= 0 || width <= 0 || height <= 0)
+            {
+                MessageBox.Show("Размеры должны быть больше нуля.");
+                return;
+            }
+
+            double floorArea = length * width;
+            double wallArea = 2 * (length + width) * height;
+
+            double floorFeet = floorArea * 10.7639;
+            double wallFeet = wallArea * 10.7639;
+
+            double laminate = floorArea;
+            double tile = floorArea + wallArea;
+            double wallpaper = wallArea;
+            // Материалы (ft²)
+            double laminateFeet = laminate * 10.7639;
+            double tileFeet = tile * 10.7639;
+            double wallpaperFeet = wallpaper * 10.7639;
+
+            MessageBox.Show(
+                $"=== ПЛОЩАДИ ===\n\n" +
+                $"Пол: {floorArea:F2} м² ({floorFeet:F2} ft²)\n" +
+                $"Стены: {wallArea:F2} м² ({wallFeet:F2} ft²)\n\n" +
+
+                $"=== РЕКОМЕНДАЦИИ ПО МАТЕРИАЛАМ ===\n\n" +
+
+                $"Ламинат (только пол):\n" +
+                $"{laminate:F2} м² ({laminateFeet:F2} ft²)\n\n" +
+
+                $"Плитка (пол + стены):\n" +
+                $"{tile:F2} м² ({tileFeet:F2} ft²)\n\n" +
+
+                $"Обои (только стены):\n" +
+                $"{wallpaper:F2} м² ({wallpaperFeet:F2} ft²)"
+            );
+
+
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
